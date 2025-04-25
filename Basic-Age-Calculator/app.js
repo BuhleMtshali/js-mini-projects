1257ab675a7a3a23a2cd868d6c60284ee6357225
Buhle <mtshalizanobuhle7@gmail.com>
2025-04-25 16:16:21 +0200

    updated the stylesheet

--- Basic-Age-Calculator/app.js
+++ Basic-Age-Calculator/app.js
@@ -2,10 +2,11 @@
 const toggleBtn = document.querySelector('#toggle-calculator');
 const calculatorWrapper = document.querySelector('.calculator-wrapper')
 toggleBtn.addEventListener('click', () => {
-    calculatorWrapper.classList.toggle('hidden')
+    calculatorWrapper.classList.toggle('hidden');
+    //changing the text of the button
     if(calculatorWrapper.classList.contains('hidden')){
         toggleBtn.textContent = 'Calculate Age'
     } else {
-        toggleBtn.textContent = 'Close Calculator'
+        toggleBtn.textContent = 'Close Calculator';
     }
 })
