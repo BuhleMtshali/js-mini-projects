b816d1ada96801543eb211dab5cc1871bfbd2624
Buhle <mtshalizanobuhle7@gmail.com>
2025-04-25 15:02:17 +0200

    updated the javascript file

--- Basic-Age-Calculator/app.js
+++ Basic-Age-Calculator/app.js
@@ -3,4 +3,9 @@ const toggleBtn = document.querySelector('#toggle-calculator');
 const calculatorWrapper = document.querySelector('.calculator-wrapper')
 toggleBtn.addEventListener('click', () => {
     calculatorWrapper.classList.toggle('hidden')
+    if(calculatorWrapper.classList.contains('hidden')){
+        toggleBtn.textContent = 'Calculate Age'
+    } else {
+        toggleBtn.textContent = 'Close Calculator'
+    }
 })
