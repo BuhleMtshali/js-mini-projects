3804750d0a34114f254168edddc7c96f188a073c
Buhle <mtshalizanobuhle7@gmail.com>
2025-05-06 00:31:33 +0200

    updated the javascript file

--- contact-book(arrays and objects)/app.js
+++ contact-book(arrays and objects)/app.js
@@ -1,11 +1,15 @@
 let contactBook = [];
+let name = document.querySelector("#name").value;
+let lastName = document.querySelector("#lastName").value;
+let number = document.querySelector("#number").value;
+
+document.querySelector("#form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    let contact = {
+        userName: name,
+        userLastName: lastName,
+        userNumber: number
+    }
+})
 
 
-let contact = {
-    userName: name,
-    userLastName: lastName,
-    userNumber: number
-}
-
-contactBook.push(contact);
-console.log(contact)
